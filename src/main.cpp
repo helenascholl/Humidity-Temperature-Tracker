ba8968378a82ae421b9051ff128e27e9daac79a1
schollsebastian <scholl.sebastian@gmx.at>
2021-08-18 17:43:46 +0200

    Read temperature and humidity

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+#include <DHT.h>
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  Serial.println(humidity);
+  Serial.println(temperature);
+
+  delay(500);
 }
